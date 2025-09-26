2380ba6555fef3c72647357cdb50190476dd2ee6
Noamaan Mulla <noamaan.mulla03@gmail.com>
2025-09-26 12:31:13 +0000

    Fix Backend

--- server/server.js
+++ server/server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const routes = require('./routes');
+const routes = require('./routes/api');
 
 const app = express();
-mongoose.connect('mongodb+srv://<username>:<password>@cluster0.mongodb.net/<dbname>?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect('mongodb+srv://noamaanmulla03_db_user:9g03TAHAFR3EAU8Q@cluster0.ym9552n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(3333);
+app.listen(3333, () => {
+    console.log('Server is running on port 3333');
+});
